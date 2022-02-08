976c20782aa65eeac96e50e1bf46012562e2c918
realme1 <realmwapp9@gmail.com>
2022-02-08 16:16:14 +0530

    abc added

--- src/main/java/com/spring/spring1/abc.java
+++ src/main/java/com/spring/spring1/abc.java
@@ -1,4 +1,14 @@
 package com.spring.spring1;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class abc {
+    @GetMapping("/") public String def(){return "sucessfully working";}
+
+    @GetMapping(path = "/home")
+    String home(){
+        return "welcome";
+    }
 }
